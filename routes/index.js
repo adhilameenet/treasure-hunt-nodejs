fe7fc0588875937080afed8af8f109e19167aff7
adhilameenet <adhilameenet@gmail.com>
2022-04-18 14:05:51 +0530

    final

--- routes/index.js
+++ routes/index.js
@@ -35,6 +35,10 @@ router.get('/re32f' , (req,res) => {
   res.render('re32f')
 })
 
+router.get('/tqs4d' , (req,res) => {
+  res.render('tqs4d')
+})
+
 
 
 
@@ -64,13 +68,23 @@ router.post('/third' , (req,res) => {
   const reqAnswer = 'samuel taylor coleridge';
   const answer = req.body.answer.toLowerCase()
   if(reqAnswer == answer) {
-    res.render('k3j2e')
+    res.render('tqs4d')
   } else {
     res.redirect('/hj4k2')
   }
 })
 
 router.post('/fourth' , (req,res) => {
+  const reqAnswer = 'beeran sahib';
+  const answer = req.body.answer.toLowerCase()
+  if(reqAnswer == answer) {
+    res.render('k3j2e')
+  } else {
+    res.redirect('/tqs4d')
+  }
+})
+
+router.post('/fifth' , (req,res) => {
   const reqAnswer = 'unesco';
   const answer = req.body.answer.toLowerCase()
   if(reqAnswer == answer) {
@@ -80,7 +94,7 @@ router.post('/fourth' , (req,res) => {
   }
 })
 
-router.post('/fifth' , (req,res) => {
+router.post('/sixth' , (req,res) => {
   const reqAnswer = 'ekata';
   const answer = req.body.answer.toLowerCase()
   if(reqAnswer == answer) {
@@ -90,7 +104,7 @@ router.post('/fifth' , (req,res) => {
   }
 })
 
-router.post('/sixth' , (req,res) => {
+router.post('/seventh' , (req,res) => {
   const answer = 1959;
   if(req.body.answer == answer) {
     res.render('re32f')
